df699dbd2735e1a9c6f3e09205a53c052d2b5c45
ForelaxX <helloworldmkv@gmail.com>
2020-06-27 19:18:56 +0800

    8.1.2.1

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,7 +5,27 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('登录页面'),
+        child: Column(
+          children: <Widget>[
+            // 利用 Expanded 来让两个 child 组件占满整个屏幕
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+                child: Center(
+                  child: Text('top'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.red,
+                child: Center(
+                  child: Text('bottom'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
