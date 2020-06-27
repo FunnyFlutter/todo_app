77bc860c5948ca0871d2d622ca014fcb41bac489
ForelaxX <helloworldmkv@gmail.com>
2020-06-27 19:34:05 +0800

    8.3.1

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -4,86 +4,95 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                color: Colors.white,
-                child: Center(
-                  child: FractionallySizedBox(
-                    child: Image.asset('assets/images/mark.png'),
-                    widthFactor: 0.4,
-                    heightFactor: 0.4,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          //利用MediaQuery来获取屏幕的高度
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height,
+          ),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    color: Colors.white,
+                    child: Center(
+                      child: FractionallySizedBox(
+                        child: Image.asset('assets/images/mark.png'),
+                        widthFactor: 0.4,
+                        heightFactor: 0.4,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(left: 24, right: 24, bottom: 12),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                          TextField(
-                            decoration: InputDecoration(
-                              hintText: '请输入邮箱',
-                              labelText: '邮箱',
-                            ),
-                          ),
-                          TextField(
-                            decoration: InputDecoration(
-                              hintText: '请输入六位以上的密码',
-                              labelText: '密码',
-                            ),
-                            obscureText: true,
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        left: 24,
-                        right: 24,
-                        top: 12,
-                        bottom: 12,
-                      ),
-                      child: FlatButton(
-                        child: Text(
-                          '登录',
-                          style: TextStyle(
-                            color: Colors.white,
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Padding(
+                          padding:
+                              EdgeInsets.only(left: 24, right: 24, bottom: 12),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                              TextField(
+                                decoration: InputDecoration(
+                                  hintText: '请输入邮箱',
+                                  labelText: '邮箱',
+                                ),
+                              ),
+                              TextField(
+                                decoration: InputDecoration(
+                                  hintText: '请输入六位以上的密码',
+                                  labelText: '密码',
+                                ),
+                                obscureText: true,
+                              ),
+                            ],
                           ),
                         ),
-                        disabledColor: Color.fromRGBO(69, 202, 160, 0.5),
-                        color: Color.fromRGBO(69, 202, 181, 1),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                          left: 24, right: 24, top: 12, bottom: 12),
-                      child: Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text('没有账号？'),
-                            InkWell(
-                              child: Text('立即注册'),
-                              onTap: () {},
+                        Padding(
+                          padding: EdgeInsets.only(
+                            left: 24,
+                            right: 24,
+                            top: 12,
+                            bottom: 12,
+                          ),
+                          child: FlatButton(
+                            child: Text(
+                              '登录',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
                             ),
-                          ],
+                            disabledColor: Color.fromRGBO(69, 202, 160, 0.5),
+                            color: Color.fromRGBO(69, 202, 181, 1),
+                          ),
                         ),
-                      ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: 24, right: 24, top: 12, bottom: 12),
+                          child: Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text('没有账号？'),
+                                InkWell(
+                                  child: Text('立即注册'),
+                                  onTap: () {},
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
