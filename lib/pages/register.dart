7f76b53b79573d8f89c500169ea3434eae6381be
ForelaxX <helloworldmkv@gmail.com>
2020-06-27 19:52:56 +0800

    9.1.0.1

---
+++ lib/pages/register.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('注册页面'),
+      ),
+    );
+  }
+}
