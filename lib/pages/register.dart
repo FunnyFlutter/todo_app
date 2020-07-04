64904d460b724580e1667187f4397b6edf372553
ForelaxX <helloworldmkv@gmail.com>
2020-07-04 20:15:03 +0800

    9.1.3.3

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final RegisterPageArgument argument =
+        ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
       body: Center(
         child: GestureDetector(
-          child: Text('注册页面'),
+          child: Text('注册页面，从${argument.className}-${argument.url}跳转而来'),
           onTap: () => Navigator.of(context).pop(),
         ),
       ),
