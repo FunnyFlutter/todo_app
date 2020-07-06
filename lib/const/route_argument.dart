285cb01de87e2882fe82ac1040c3a93430ed92b3
ForelaxX <helloworldmkv@gmail.com>
2020-07-06 23:08:51 +0800

    10.3.2.2

--- lib/const/route_argument.dart
+++ lib/const/route_argument.dart
@@ -1,6 +1,21 @@
+import 'package:todo_list/model/todo.dart';
+
 class RegisterPageArgument {
   final String className;
   final String url;
 
   RegisterPageArgument(this.className, this.url);
+}
+
+enum OpenType {
+  Add,
+  Edit,
+  Preview,
+}
+
+class EditTodoPageArgument {
+  final OpenType openType;
+  final Todo todo;
+
+  EditTodoPageArgument({this.openType, this.todo});
 }
--- lib/const/route_url.dart
+++ lib/const/route_url.dart
@@ -1,3 +1,4 @@
 const LOGIN_PAGE_URL = '/login';
 const REGISTER_PAGE_URL = '/register';
 const TODO_ENTRY_PAGE_URL = '/entry';
+const EDIT_TODO_PAGE_URL = '/edit';
