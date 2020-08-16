bfb8b81c3dd64bbfa87cdbccb04adce839f3954b
ForelaxX <helloworldmkv@gmail.com>
2020-08-16 11:33:56 +0800

    12.3.3.1

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,6 +5,23 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+                                          methodChannelWithName:@"com.funny_flutter.todo_list.channel"
+                                          binaryMessenger:controller.binaryMessenger];
+
+  [channel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+      if ([call.method isEqualToString:@"getCurrentLocation"]) {
+          result(@{
+              @"latitude": @"39.92",
+              @"longitude": @"116.46",
+              @"description": @"北京",
+          });
+      } else {
+          result(FlutterMethodNotImplemented);
+      }
+  }];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
