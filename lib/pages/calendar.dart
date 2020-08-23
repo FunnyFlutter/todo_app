46122f2bf77e7713ef76fd986c14b14a917b68fb
ForelaxX <helloworldmkv@gmail.com>
2020-08-23 11:25:21 +0800

    13.1.2.5

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:todo_list/const/route_argument.dart';
+import 'package:todo_list/const/route_url.dart';
 import 'package:todo_list/extension/date_time.dart';
 import 'package:todo_list/model/todo.dart';
 import 'package:todo_list/model/todo_list.dart';
@@ -52,17 +54,92 @@ class _CalendarPageState extends State<CalendarPage> {
     _todosToShow.addAll(_date2TodoMap[_initialDay] ?? []);
   }
 
+  void _onTap(Todo todo) async {
+    Todo changedTodo = await Navigator.of(context).pushNamed(EDIT_TODO_PAGE_URL,
+        arguments: EditTodoPageArgument(openType: OpenType.Preview, todo: todo));
+    if (changedTodo == null) {
+      return;
+    }
+    _todoList.update(todo);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('日历'),
       ),
-      body: Center(
-        child: Text(
-          this.runtimeType.toString(),
-        ),
+      body: Column(
+        children: <Widget>[
+          TableCalendar(
+            calendarController: _calendarController,
+            locale: 'zh_CN',
+            events: _date2TodoMap,
+            headerStyle: HeaderStyle(),
+            calendarStyle: CalendarStyle(
+              todayColor: Colors.transparent,
+              todayStyle: TextStyle(color: Colors.black),
+            ),
+            initialSelectedDay: _initialDay,
+            onDaySelected: (DateTime day, List events) {
+              this.setState(() {
+                _todosToShow = events.cast<Todo>();
+              });
+            },
+          ),
+          Expanded(
+            child: _buildTaskListArea(),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _buildTaskListArea() {
+    return ListView.builder(
+      itemCount: _todosToShow.length,
+      itemBuilder: (context, index) {
+        Todo todo = _todosToShow[index];
+        return GestureDetector(
+          onTap: () => _onTap(todo),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    color: todo.status.color,
+                    height: 10,
+                    width: 10,
+                    margin: EdgeInsets.all(10),
+                  ),
+                  Text(todo.title),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.access_time,
+                      size: 15,
+                      color: Color(0xffb9b9bc),
+                    ),
+                    Text(
+                      ' ${todo.startTime.hour} - ${todo.endTime.hour}',
+                      style: TextStyle(color: Color(0xffb9b9bc)),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 1,
+                color: Color(0xffececed),
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
