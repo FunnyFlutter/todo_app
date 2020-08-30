9d9ae4fe3a00f8c4d57ed3fef6302648e47f5a00
ForelaxX <helloworldmkv@gmail.com>
2020-08-30 17:41:15 +0800

    13.4.2.1

--- lib/model/network_client.dart
+++ lib/model/network_client.dart
@@ -90,4 +90,34 @@ class NetworkClient {
     }
     return result['error'];
   }
+
+  Future<FetchListResult> fetchList(String userKey) async {
+    FetchListResult result;
+    try {
+      Response response = await get(
+        '$baseUrl/list?userKey=$userKey',
+        headers: commonHeaders,
+      );
+      result = FetchListResult.fromJson(JsonDecoder().convert(response.body));
+    } catch (e) {
+      result = FetchListResult(error: '服务器请求失败，请检查网络连接');
+    }
+    return result;
+    ;
+  }
+}
+
+class FetchListResult {
+  final List<Todo> data;
+  final DateTime timestamp;
+  final String error;
+
+  FetchListResult({this.data, this.timestamp, this.error = ''});
+
+  factory FetchListResult.fromJson(Map<dynamic, dynamic> json) {
+    return FetchListResult(
+      data: json['data']['data'].map<Todo>((e) => Todo.fromMap(e)).toList(),
+      timestamp: DateTime.fromMicrosecondsSinceEpoch(json['data']['timestamp']),
+    );
+  }
 }
